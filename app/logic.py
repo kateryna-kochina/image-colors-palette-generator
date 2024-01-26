ae4d1f8f06d223738ba4e709e3480303126b6206
kateryna.kochina <kochina.ev@gmail.com>
2024-01-26 17:11:00 -0500

    Add results table

--- app/logic.py
+++ app/logic.py
@@ -1,12 +1,18 @@
 from collections import Counter
 from PIL import Image
+from operator import itemgetter
+
+def rgb_to_hex(rgb):
+    # Convert RGB to hexadecimal
+    hex_color = "#{:02x}{:02x}{:02x}".format(*rgb)
+    return hex_color
 
 
 def get_top_colors(image_path, num_colors=10):
     # Load the image
     image = Image.open(image_path)
 
-    # Convert image to RGB (if not already in RGB mode)
+    # Convert image to RGB
     image = image.convert('RGB')
 
     # Get the image pixels
@@ -15,7 +21,13 @@ def get_top_colors(image_path, num_colors=10):
     # Count the occurrences of each color
     color_counts = Counter(pixels)
 
-    # Get the top 10 colors
-    top_colors = color_counts.most_common(num_colors)
+    # Calculate the total number of pixels in the image
+    total_pixels = len(pixels)
+
+    # Calculate the percentage of each color's presence
+    color_percentages = [(rgb_to_hex(color), round(count / total_pixels * 100, 6)) for color, count in color_counts.items()]
+
+    # Get the top N colors, sort descending by color percentage
+    top_colors = sorted(color_percentages, key=itemgetter(1), reverse=True)[:num_colors]
 
     return top_colors
